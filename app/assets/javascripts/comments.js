484c73ffa3f4ffe63d6d0c9da11fa7722a305e78
rudy <unixc3t@gmail.com>
2017-09-22 12:46:56 +0800

    fix three

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,7 +1,6 @@
 $(function () {
 
   $('form#new_comment').on('ajax:success', function (event, response, status) {
-    let zz = $(event.currentTarget);
     if (response.errors) {
       $('.comment-errors').html('');
       for (let field in response.errors) {
@@ -17,6 +16,7 @@ $(function () {
       let $el = $('<li></li>');
       $el.text(response.nick + ':' + response.text);
       $el.appendTo($("#commnets-list"));
+      let zz = $(event.currentTarget);
       zz.find('[name="comment[text]"]').val('');
       zz.find('[name="comment[nick]"]').val(response.nick);
       window.location.href = window.location.href;
